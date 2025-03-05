109119c76bb6d360a936e323f0938fe04ea5e7c4
Sanskar Wankhede <sanskarw10@gmail.com>
2025-03-05 15:42:23 +0530

    second

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app').then(()=>{
-       console.log('DB connected') ;
-    })
-}
+const MONGO_URL = "mongodb://localhost:27017/food-app";
+export const connectDB = async () => {
+  await mongoose.connect(MONGO_URL).then(() => {
+    console.log("DB connected");
+  });
+};
